0731c67cf4693ec8fff9ff8581df422400b2696d
krzysiu <i22sachanek@gmail.com>
2025-09-27 21:36:29 +0200

    Pierwsza wersja Asystenta Głosowego

---
+++ app/src/main/java/com/example/asystentglosowy/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.asystentglosowy
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
